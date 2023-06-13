fb0cde4b1c2502b28aae34daba5b29562f1f666b
Ryan Phillips <ryanmphill@gmail.com>
2023-06-13 10:43:31 -0500

    Added functionality to dropdown menu with a link to logout

--- src/components/nav/DropdownMenu.js
+++ src/components/nav/DropdownMenu.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './DropdownMenu.css' // Import CSS styles for the dropdown menu
+import { Link, useNavigate } from 'react-router-dom';
 
 export const DropdownMenu = () => {
     const [isOpen, setIsOpen] = useState(false) // State to track if the dropdown is open or closed
@@ -8,33 +9,56 @@ export const DropdownMenu = () => {
       setIsOpen(!isOpen) // Toggle the state to open or close the dropdown
     }
 
-    // Untoggle the dropdown when the user clicks anywhere else 
-    /*
+    // Function to untoggle the dropdown when the user clicks anywhere else 
+    const handleOutsideClick = (event) => {
+        const dropdownContainer = document.querySelector('.dropdown')
+        if (!dropdownContainer.contains(event.target)) {
+            setIsOpen(false)
+        }
+    }
+    /* Observe when dropdown state changes between open and closed, then 
+      add and remove event listener to run handleOutsideClick callback function*/
     useEffect(
         () => {
             if (isOpen) {
-                const handleOutsideClick = (event) => {
-                    if (event.target.classList.contains("dropdown-content") === false) {
-                        setIsOpen(false)
-                    }
-                }
-    
+                /* When the dropdown is open, add an event listener that
+                   closes the dropdown when the user clicks anywhere else */
                 document.addEventListener('click', handleOutsideClick)
-            }
+            } else {
+                // When dropdown is closed, remove event listener
+                document.removeEventListener('click', handleOutsideClick)
+            } 
+            return () => {
+                /* Remove the event listener everytime the component is rendered
+                   with a cleanup function */
+                document.removeEventListener('click', handleOutsideClick)
+            } 
         },
         [isOpen]
     )
-  */
+
+    //Import useNavigate and assign it to a variable
+    const navigate = useNavigate()
+
     return (
       <div className="dropdown">
         <button className="dropdown-button" onClick={toggleDropdown}>
           Menu
         </button>
         {isOpen && <>
-            <div className="dropdown-content">
-            <a href="#">Clickable Link</a>
-            </div>
-            <div className="dropdown-content"></div>
+            <section className="dropdown-content">
+              <Link className="dropdown--link" to="">Clickable link</Link>
+              { // Logout button
+                localStorage.getItem("gastro_user")
+                  ? <div className="navbar__menuItem navbar__logout">
+                    <Link className="dropdown--link" to="" onClick={() => {
+                      localStorage.removeItem("gastro_user")
+                      navigate("/", { replace: true })
+                    }}>Logout</Link>
+                  </div>
+                  : ""
+              }
+            </section>
         </> 
         }
       </div>
--- src/components/nav/UserNav.js
+++ src/components/nav/UserNav.js
@@ -1,22 +1,21 @@
-import { Link, useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 import "./NavBar.css"
+import { DropdownMenu } from "./DropdownMenu"
 
 export const UserNav = () => {
-    const navigate = useNavigate()
+    
 
     return (
         <ul className="navbar">
-            
-            {
-                localStorage.getItem("gastro_user")
-                    ? <li className="navbar__item navbar__logout">
-                        <Link className="navbar__link" to="" onClick={() => {
-                            localStorage.removeItem("gastro_user")
-                            navigate("/", {replace: true})
-                        }}>Logout</Link>
-                    </li>
-                    : ""
-            }
+            <li className="navbar__item">
+                <Link to="/">Logo will go here</Link>
+            </li>
+            <li className="navbar__item navbar__Title">
+                
+            </li>
+            <li className="navbar__item navbar__menu">
+                <DropdownMenu />
+            </li>
         </ul>
     )
 }
