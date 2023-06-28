f61201031c7de1f98cb22bbfd4a9915de59d8c42
Ryan Phillips <ryanmphill@gmail.com>
2023-06-28 10:30:31 -0500

    Added logo and hamburger icon to user navbar and added hover effect to hamburger icon

--- src/components/nav/AdminNav.js
+++ src/components/nav/AdminNav.js
@@ -1,16 +1,16 @@
-import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import "./NavBar.css"
 import { DropdownMenu } from "./DropdownMenu"
 import gastroLogo from "../../assets/Chef_black.svg"
 
 export const AdminNav = ( {currentUser} ) => {
-    
+    const navigate = useNavigate()
 
     return (
         <ul className="navbar">
             <li id="logoContainer">
-                <img className="navbar__logo" src={gastroLogo} alt="Logo"></img>
-                <h2 id="gastroTitle">Gastro Gnome</h2>
+                <img className="navbar__logo" src={gastroLogo} alt="Logo" onClick={() => navigate("/")}></img>
+                <h2 id="gastroTitle" onClick={() => navigate("/")}>Gastro Gnome</h2>
             </li>
             <li className="navbar__item navbar__Title">
                 
--- src/components/nav/UserNav.js
+++ src/components/nav/UserNav.js
@@ -1,14 +1,16 @@
-import { Link } from "react-router-dom"
 import "./NavBar.css"
 import { DropdownMenu } from "./DropdownMenu"
+import gastroLogo from "../../assets/Chef_black.svg"
+import { useNavigate } from "react-router-dom"
 
 export const UserNav = ( {currentUser} ) => {
-    
+    const navigate = useNavigate()
 
     return (
         <ul className="navbar">
-            <li className="navbar__item">
-                <h3>Gastro Gnome</h3>
+            <li id="logoContainer">
+                <img className="navbar__logo" src={gastroLogo} alt="Logo" onClick={() => navigate("/")}></img>
+                <h2 id="gastroTitle" onClick={() => navigate("/")}>Gastro Gnome</h2>
             </li>
             <li className="navbar__item navbar__Title">
                 
