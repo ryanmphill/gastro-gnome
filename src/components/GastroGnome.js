25ecf5f281df3735c8ef10e88cc318c1bf153b23
Ryan Phillips <ryanmphill@gmail.com>
2023-07-05 16:13:52 -0500

    Added useEffect to show user top of page when navigated to new path

--- src/components/GastroGnome.js
+++ src/components/GastroGnome.js
@@ -1,13 +1,23 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useLocation } from "react-router-dom"
 import { Authorized } from "./views/Authorized"
 import { ApplicationViews } from "./views/ApplicationViews"
 import { Login } from "./auth/Login"
 import { Register } from "./auth/Register"
 import "./GastroGnome.css"
 import { NavBar } from "./nav/NavBar"
+import { useEffect } from "react"
 
 
 export const GastroGnome = () => {
+	const { pathname } = useLocation()
+	// Navigate user to top when navigating to a new page
+	useEffect(
+		() => {
+			window.scrollTo(0, 0)
+		},
+		[pathname]
+	)
+	
 	return <Routes>
 		<Route path="/login" element={<Login />} />
 		<Route path="/register" element={<Register />} />
