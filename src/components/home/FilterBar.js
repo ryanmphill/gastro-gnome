fb2c40d0d4a66837f723731be847a8ec04e84cd1
Ryan Phillips <ryanmphill@gmail.com>
2023-07-11 23:41:04 -0500

    Moved state for tracking filter options into FilterBar component to remove excess props

--- src/components/home/FilterBar.js
+++ src/components/home/FilterBar.js
@@ -1,8 +1,22 @@
 import { SearchRecipes } from "./SearchRecipes";
 import { FilterByCategories } from "./FilterByCategories";
 import { SelectedCategories } from "./SelectedCategories";
+import { useState } from "react";
 
-export const FilterBar = ({ searchTerms, updateSearchTerms, setFilteredRecipes, recipes, onlyRecipesWithTags, updateOnlyRecipesWithTags, onlySearchedRecipes, updateOnlySearchedRecipes, chosenCategories, updateChosenCategories }) => {
+export const FilterBar = ({ setFilteredRecipes, recipes }) => {
+
+    // State to track user input in search bar
+    const [searchTerms, updateSearchTerms] = useState("")
+    // State to keep track of all selected categories the user wants to use to filter recipe feed
+    const [chosenCategories, updateChosenCategories] = useState([])
+
+    /*  Define additional state variables to allow users to stack and unstack filtering options.
+        Once the 'recipesToDisplay' is set, separate state will be preserved for filtering by search
+        query and filtering by category tag. This will allow the user remove their search and still
+        view filtered recipes based on category, or remove the selected category filters and still
+        view results based on their search. */
+    const [onlyRecipesWithTags, updateOnlyRecipesWithTags] = useState([])
+    const [onlySearchedRecipes, updateOnlySearchedRecipes] = useState([])
 
     return <section className="filterContainer"> 
         <section className="filterBar">
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -14,32 +14,19 @@ export const Home = () => {
     // State for all recipes
     const [recipes, setRecipes] = useState([]) // Observing initial state []
 
+
+    /*--FILTER-OPTIONS--------------------------------------------------------------------------*/
     /* State for which posts to display at the broadest level. User can choose
        'discover', which shows 'allPosts', or 'my feed' which shows 'postsFollowed' - 
        posts from only users the current user is 'following' */
     const [display, setDisplay] = useState("allPosts") 
     // State for recipes that will be displayed based on users selection of 'display'
     const [recipesToDisplay, updateRecipesToDisplay] = useState([])
-    // State for recipes filtered by either searching or filtering by category
-
-    /*--FILTER-OPTIONS--------------------------------------------------------------------------*/ 
     // State to allow recipes to be filtered by search query or by selected category tag
     const [filteredRecipes, setFilteredRecipes] = useState([])
-    // State to track user input in search bar
-    const [searchTerms, updateSearchTerms] = useState("")
-    // State to keep track of all selected categories the user wants to use to filter recipe feed
-    const [chosenCategories, updateChosenCategories] = useState([])
-
-    /*  Define additional state variables to allow users to stack and unstack filtering options.
-        Once the 'recipesToDisplay' is set, separate state will be preserved for filtering by search
-        query and filtering by category tag. This will allow the user remove their search and still
-        view filtered recipes based on category, or remove the selected category filters and still
-        view results based on their search. */
-    const [onlyRecipesWithTags, updateOnlyRecipesWithTags] = useState([])
-    const [onlySearchedRecipes, updateOnlySearchedRecipes] = useState([])
     /*-------------------------------------------------------------------------------------------*/
 
-    /*---------------------------------------------------------------------------------------------------------*/
+    /*-GET RECIPES FETCH CALL-------------------------------------------------------------------------------*/
     // Fetch the list of recipes with user info expanded and ingredients and categories embedded
     const fetchRecipes = () => {
         fetch(`http://localhost:8088/recipeCards?_expand=user&_embed=ingredientsInRecipes&_embed=categoriesOfRecipes`)
@@ -57,7 +44,7 @@ export const Home = () => {
     )
     /*-----------------------------------------------------------------------------------------------------*/
 
-    /*-----------------------------------------------------------------------------------------------------*/
+    /*-GET CURRENT USER'S FOLLOW DATA-----------------------------------------------------------------------*/
     // Maintain 'follows' state here so that all listed recipes are updated when user is followed/unfollowed
     // Set a state variable for the user's follows
     const [usersFollows, updateUsersFollows] = useState([])
@@ -84,6 +71,8 @@ export const Home = () => {
     // Assign a variable to useNavigate()
     const navigate = useNavigate()
     
+    /* Render discover/my-feed tab, filter bar for searching/filtering by category, post recipe button,
+       and recipe feed */
     return <section className="pageBody">
 
         <FeedChoice recipes={recipes}
@@ -95,16 +84,8 @@ export const Home = () => {
 
         <div className="feedControl">
 
-            <FilterBar searchTerms={searchTerms}
-                updateSearchTerms={updateSearchTerms}
-                setFilteredRecipes={setFilteredRecipes}
-                recipes={recipesToDisplay}
-                onlyRecipesWithTags={onlyRecipesWithTags}
-                updateOnlyRecipesWithTags={updateOnlyRecipesWithTags}
-                onlySearchedRecipes={onlySearchedRecipes}
-                updateOnlySearchedRecipes={updateOnlySearchedRecipes}
-                chosenCategories={chosenCategories}
-                updateChosenCategories={updateChosenCategories} />
+            <FilterBar setFilteredRecipes={setFilteredRecipes}
+                recipes={recipesToDisplay} />
 
 
             {
